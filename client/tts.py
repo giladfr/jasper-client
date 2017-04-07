b28bbdb3005056a125d06a517b254acaf53b1fee
Gilad Fride <giladfr@gmail.com>
2017-04-07 12:04:18 +0300

    Added local ivona exec to TTS engines

--- client/tts.py
+++ client/tts.py
@@ -417,6 +417,82 @@ class PicoTTS(AbstractTTSEngine):
         os.remove(fname)
 
 
+class IvonaLocalTTS(AbstractTTSEngine):
+    """
+    Uses the ivona local speech synthesizer
+    Requires ivona binary to be installed locally
+    """
+
+    SLUG = "ivonalocal-tts"
+
+    def __init__(self, language="en-US"):
+        super(self.__class__, self).__init__()
+        self.language = language
+
+    @classmethod
+    def is_available(cls):
+        return (super(cls, cls).is_available() and
+                diagnose.check_executable('ivona_tts'))
+
+    @classmethod
+    def get_config(cls):
+        # FIXME: Replace this as soon as we have a config module
+        config = {}
+        # HMM dir
+        # Try to get hmm_dir from config
+        profile_path = jasperpath.config('profile.yml')
+        if os.path.exists(profile_path):
+            with open(profile_path, 'r') as f:
+                profile = yaml.safe_load(f)
+                if 'pico-tts' in profile and 'language' in profile['pico-tts']:
+                    config['language'] = profile['pico-tts']['language']
+
+        return config
+
+    # @property
+    # def languages(self):
+    #     cmd = ['pico2wave', '-l', 'NULL',
+    #                         '-w', os.devnull,
+    #                         'NULL']
+    #     with tempfile.SpooledTemporaryFile() as f:
+    #         subprocess.call(cmd, stderr=f)
+    #         f.seek(0)
+    #         output = f.read()
+    #     pattern = re.compile(r'Unknown language: NULL\nValid languages:\n' +
+    #                          r'((?:[a-z]{2}-[A-Z]{2}\n)+)')
+    #     matchobj = pattern.match(output)
+    #     if not matchobj:
+    #         raise RuntimeError("pico2wave: valid languages not detected")
+    #     langs = matchobj.group(1).split()
+    #     return langs
+
+    def say(self, phrase):
+        self._logger.debug("Saying '%s' with '%s'", phrase, self.SLUG)
+        with tempfile.NamedTemporaryFile(suffix='.wav', delete=False) as f:
+            fname = f.name
+        cmd = ['ivona_tts', 'EN_us','Mark',phrase, fname]
+        self._logger.debug('Executing %s', ' '.join([pipes.quote(arg)
+                                                     for arg in cmd]))
+        with tempfile.TemporaryFile() as f:
+            subprocess.call(cmd, stdout=f, stderr=f)
+            f.seek(0)
+            output = f.read()
+            if output:
+                self._logger.debug("Output was: '%s'", output)
+        self.play(fname)
+        os.remove(fname)
+
+    def play(self, filename):
+        # FIXME: Use platform-independent audio-output here
+        # See issue jasperproject/jasper-client#188
+        cmd = ['aplay','-fS16_LE', str(filename)]
+        self._logger.debug('Executing %s', ' '.join([pipes.quote(arg)
+                                                     for arg in cmd]))
+        subprocess.call(cmd)
+
+
+
+
 class GoogleTTS(AbstractMp3TTSEngine):
     """
     Uses the Google TTS online translator
@@ -704,8 +780,10 @@ if __name__ == '__main__':
     for i, engine in enumerate(disabled_engines, start=1):
         print("%d. %s" % (i, engine.SLUG))
 
-    print("")
-    for i, engine in enumerate(available_engines, start=1):
-        print("%d. Testing engine '%s'..." % (i, engine.SLUG))
-        engine.get_instance().say("This is a test.")
-    print("Done.")
+    available_engines[5].get_instance().say("This is a test.")
+
+    # print("")
+    # for i, engine in enumerate(available_engines, start=1):
+    #     print("%d. Testing engine '%s'..." % (i, engine.SLUG))
+    #     engine.get_instance().say("This is a test.")
+    # print("Done.")
